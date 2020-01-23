1b62d29bad7974b214b434d6e7a65ab695c01f1e
Chase Clarke <chaseclarke@Chase-2.local>
2020-01-23 11:51:53 -0500

    added unit tests

---
+++ _test.py
@@ -0,0 +1,20 @@
+import pytest
+import convertToRoman
+
+def test_4():
+    assert convertToRoman.convert(4) == "IV"
+
+def test_5():
+    assert convertToRoman.convert(5) == "V"
+
+def test_10():
+	assert convertToRoman.convert(10) == "X"
+
+def test_35():
+	assert convertToRoman.convert(35) == "XXXV"
+
+def test_994():
+	assert convertToRoman.convert(994) == "CMXCIV"
+
+def test_1995():
+	assert convertToRoman.convert(1995) == "MCMXCV"
---
+++ convertToRoman.py
@@ -0,0 +1,18 @@
+romanKey = [(1000, 'M'), (900, 'CM'), (500, 'D'),
+			(400, 'CD'), (100, 'C'), (90, 'XC'),
+            (50, 'L'), (40, 'XL'), (10, 'X'), 
+            (9, 'IX'), (5, 'V'), (4, 'IV'), 
+            (1, 'I')]
+
+
+def convert(arabic):
+
+    roman = ''
+
+    while arabic > 0:
+        for a, r in romanKey:
+            while arabic >= a:
+                arabic -= a
+                roman += r
+
+    return roman
